2d03b3f1a3cffd29346fa43c1fbb1d1782c213c8
Bucher <ch1011547@komax.org>
2017-12-20 16:10:56 +0100

    Added Website

---
+++ Plantsite/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>SmartPlant</title>
+  <link rel="stylesheet" href="stylesheet/style.css"
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+  <div class="wrapper">
+    <h1>willkommen auf meiner Seite</h1><br>
+    <h3>Wie fühlst du dich? </h3>
+  <div class="Barometerbild1">
+  </div>
+  <input name="SehrGut"class="checkboxsehrgut" type="checkbox">
+    <input name="Gut"class="checkboxgut" type="checkbox">
+    <div class="rund">
+      <input name="Schlecht"class="checkboxschlecht" type="checkbox">
+      </div>
+    </div>
+</body>
+</html>
